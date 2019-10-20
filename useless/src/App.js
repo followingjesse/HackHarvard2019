3aa4980aa7c67bf9aeeb7c5dec3cff8a8760df4e
Jose Miguel Ballesteros <ballesjm@mcmaster.ca>
2019-10-20 05:11:05 -0400

    s

--- useless/src/App.js
+++ useless/src/App.js
@@ -5,16 +5,24 @@ import "./App.css";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      counter: 0
+    };
+    this.increment = this.increment.bind(this);
   }
-  render() {
+  render = () => {
     return (
       <div style={{ textAlign: "center" }}>
-        <button onClick={this.useless} style={{ textAlign: "center" }}>
+        <button onClick={this.increment} style={{ textAlign: "center" }}>
           Useless
         </button>
       </div>
     );
+  }
+
+  increment = () => {
+    this.state.counter = this.state.counter + 1;
+    console.log(this.state.counter);
   }
 }
 
